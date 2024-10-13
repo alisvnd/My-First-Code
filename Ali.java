3c42be992719faf4fe414128e838d9147d0b1fa7
Ali <alisvnd2005@gmail.com>
2024-10-13 15:23:22 +0300

    first commit

---
+++ Ali.java
@@ -0,0 +1,16 @@
+/**
+ * Ali
+ */
+public class Ali {
+
+public static void main(String[] args) {
+    String studentFirstName = "Ali İhsan " ;
+    String studentLastName = "Sevindi" ;
+    int section = 02 ;
+    System.out.println("Student Name : " + studentFirstName + studentLastName);
+    System.out.println("Student Section : "+section );
+}
+    
+    
+
+}
